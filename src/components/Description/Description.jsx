1e456f47406e85b77441ad7a8ddbb4a8752e79ff
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-10-21 19:34:07 +0200

    update Description

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,12 @@
+import './Description.css';
+
+const Description = () => {
+  return (
+      <div>
+          <p className="cafe-name">Sip Happens Café</p>
+          <p className="cafe-name-paragraf">Please leave your feedback about our service by selecting one of the options below.</p>
+      </div>
+  );
+};
+
+export default Description;
